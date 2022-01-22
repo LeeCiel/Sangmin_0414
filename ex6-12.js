664c8cf98cb4bedc7796ebb88968fc61047404cf
LeeCiel <zm20192@josai.ac.jp>
2022-01-22 03:14:04 +0000

    2022

---
+++ ex6-12.js
@@ -0,0 +1,8 @@
+function imgchange1(){document.getElementById('logo').src='./image/keiei_small.jpg';}
+function imgchange2(){document.getElementById('logo').src='./image/josai.jpg';}
+function imagchange3(){document.getElementById('logo').src='./image/keiei_small.jpg';}
+function startfnc(){
+    //関数hyouji()1000ミリ秒間隔で呼び戻す
+   setInterval("hyoji()",1000);}
+   //setTimeout()を含む関数を呼び出す
+   setTimeout("hyoji()",1000);
