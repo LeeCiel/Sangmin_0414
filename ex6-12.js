dfe19458a78b5992f8b59f3e0ca7da96676e4751
LeeCiel <zm20192@josai.ac.jp>
2022-01-22 05:43:48 +0000

    2022

--- ex6-12.js
+++ ex6-12.js
@@ -1,13 +1,14 @@
-function hyoji(){
+function hyoji2(){
     //表示する文字
     var str = "一文字ずつ表示します。";
     //テキストボックスの文字数
-    var cnt = document.timer.moji.value.length;
+    var cnt = document.timer2.moji2.value.length;
     //文字が全部表示されているか確認
 
 if ( cnt < 11 ){
     //現在より一文字多く切り出して表示
-    document.timer.moji.value = str.substr(0,cnt+1); }
+    document.timer2.moji2.value = str.substr(0,cnt+1); }
     else {
     //すべて表示されたら、空文字に戻す
-    document.timer.moji.value = "";}}
+    document.timer2.moji2.value = "";}}
+    setTimeout("hyoji2()",1000);
